ad8d63576a6169de8ed136daeaaf80a594e0af4e
Zarwa Aslam <zarwaaslam1010@gmail.com>
2026-08-28 14:20:24 +0500

    update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 # DATABASE CONFIGURATION
 # ============================================================
 
-DB_HOST = os.getenv("DB_HOST")
+DB_HOST = os.getenv("DB_HOST") 
 DB_PORT = int(os.getenv("DB_PORT", "26238"))
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
